dd8be2d178e227b32d3b212f627fdf8a23cd5049
Jacob Erisman <jacob.erisman@gmail.com>
2023-09-11 11:07:43 -0400

    Completed Lab - JE

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData (name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name, email})
+    })
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        const newId = data.id;
+        document.body.innerHTML += `<p>New ID: ${newId}</p>`;
+    })
+    .catch(function (error){
+        alert(error.message);
+        document.body.innerHTML += `<p>Error: ${error}</p>`;
+    })
+}
